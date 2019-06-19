25dfa7d061dd55f041eb7ba179030f70e0b2f209
Wesadan <45579240+Wesadan@users.noreply.github.com>
2019-06-19 10:41:17 -0300

    adicionado projeto Teste FizzBuzz

---
+++ FizzBuzz/UnitTest1.cs
@@ -0,0 +1,34 @@
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void GetOutput_ImputIsDisibleBy3And5_returnFizzBuzz()
+        {
+        }
+
+       
+        [SetUp]
+        public void GetOutput_ImputIsDisibleBy30Only_returnFizz()
+        {
+        }
+
+        [SetUp]
+        public void GetOutput_ImputIsDisibleBy50OnlyAnd5_returnBuzz()
+        {
+        }
+
+        [SetUp]
+        public void GetOutput_ImputIsDisibleBy3Or5_ReturnTheSameNumber()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
